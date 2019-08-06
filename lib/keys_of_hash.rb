4df8941c24fa789fc2b27e88c4dd544866f7e97f
Pete Hanner <petehanner@gmail.com>
2019-08-05 21:16:02 -0400

    Wrote new Hash method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # empty array
+    final = []
+    # iterate each hash pair
+    each do |key, value|
+      # push key into final array if value is in arguments
+      if arguments.include?(value)
+        final << key
+      end
+    end
+    # return array
+    final
   end
 end
